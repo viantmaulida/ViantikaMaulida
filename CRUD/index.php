3d9ca0b01370a7e78042558d62fdd1157a746c79
Viantika Maulida <90303995+viantmaulida@users.noreply.github.com>
2021-11-09 20:33:22 +0700

    Add files via upload

---
+++ CRUD/index.php
@@ -0,0 +1,181 @@
+<?php
+	$server = "localhost";
+	$user = "root";
+	$pass = "";
+	$database = "dblatihan";
+
+	$koneksi = mysqli_connect($server, $user, $pass, $database)or die(mysqli_error($koneksi));
+
+	if(isset($_POST['bsimpan']))
+	{
+		if($_GET['hal'] == "edit")
+		{
+			$edit = mysqli_query($koneksi, "UPDATE tmhs set
+											 	nim = '$_POST[tnim]',
+											 	nama = '$_POST[tnama]',
+												alamat = '$_POST[talamat]',
+											 	prodi = '$_POST[tprodi]'
+											 WHERE id_mhs = '$_GET[id]'
+										   ");
+			if($edit) 
+			{
+				echo "<script>
+						alert('Edit data suksess!');
+						document.location='index.php';
+				     </script>";
+			}
+			else
+			{
+				echo "<script>
+						alert('Edit data GAGAL!!');
+						document.location='index.php';
+				     </script>";
+			}
+		}
+		else
+		{
+			$simpan = mysqli_query($koneksi, "INSERT INTO tmhs (nim, nama, alamat, prodi)
+										  VALUES ('$_POST[tnim]', 
+										  		 '$_POST[tnama]', 
+										  		 '$_POST[talamat]', 
+										  		 '$_POST[tprodi]')
+										 ");
+			if($simpan) 
+			{
+				echo "<script>
+						alert('Simpan data suksess!');
+						document.location='index.php';
+				     </script>";
+			}
+			else
+			{
+				echo "<script>
+						alert('Simpan data GAGAL!!');
+						document.location='index.php';
+				     </script>";
+			}
+		}
+
+
+		
+	}
+
+
+	if(isset($_GET['hal']))
+	{
+		if($_GET['hal'] == "edit")
+		{
+			$tampil = mysqli_query($koneksi, "SELECT * FROM tmhs WHERE id_mhs = '$_GET[id]' ");
+			$data = mysqli_fetch_array($tampil);
+			if($data)
+			{
+				$vnim = $data['nim'];
+				$vnama = $data['nama'];
+				$valamat = $data['alamat'];
+				$vprodi = $data['prodi'];
+			}
+		}
+		else if ($_GET['hal'] == "hapus")
+		{
+			$hapus = mysqli_query($koneksi, "DELETE FROM tmhs WHERE id_mhs = '$_GET[id]' ");
+			if($hapus){
+				echo "<script>
+						alert('Hapus Data Suksess!!');
+						document.location='index.php';
+				     </script>";
+			}
+		}
+	}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tugas Pemweb</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+
+	<h1 class="text-center">Data Form Mahasiswa</h1>
+
+	<div class="card mt-3">
+	  <div class="card-header bg-primary text-white">
+	    Form Input Data Mahasiswa
+	  </div>
+	  <div class="card-body">
+	    <form method="post" action="">
+	    	<div class="form-group">
+	    		<label>Nim</label>
+	    		<input type="text" name="tnim" value="<?=@$vnim?>" class="form-control" placeholder="" required>
+	    	</div>
+	    	<div class="form-group">
+	    		<label>Nama</label>
+	    		<input type="text" name="tnama" value="<?=@$vnama?>" class="form-control" placeholder="" required>
+	    	</div>
+	    	<div class="form-group">
+	    		<label>Alamat</label>
+	    		<textarea class="form-control" name="talamat"  placeholder=""><?=@$valamat?></textarea>
+	    	</div>
+	    	<div class="form-group">
+	    		<label>Program Studi</label>
+	    		<select class="form-control" name="tprodi">
+	    			<option value="<?=@$vprodi?>"><?=@$vprodi?></option>
+	    			<option value="D3-Informatika">D3-Informatika</option>
+	    			<option value="S1-Sistem Informasi">S1-Sistem Informasi</option>
+	    			<option value="S1-Sistem Informasi Kelautan">S1-Sistem Informasi Kelautan</option>
+	    		</select>
+	    	</div>
+
+	    	<button type="submit" class="btn btn-success" name="bsimpan">Simpan</button>
+	    	<button type="reset" class="btn btn-danger" name="breset">Kosongkan</button>
+
+	    </form>
+	  </div>
+	</div>
+
+	<div class="card mt-3">
+	  <div class="card-header bg-success text-white">
+	    Daftar Mahasiswa
+	  </div>
+	  <div class="card-body">
+	    
+	    <table class="table table-bordered table-striped">
+	    	<tr>
+	    		<th>No.</th>
+	    		<th>Nim</th>
+	    		<th>Nama</th>
+	    		<th>Alamat</th>
+	    		<th>Program Studi</th>
+	    		<th>Aksi</th>
+	    	</tr>
+	    	<?php
+	    		$no = 1;
+	    		$tampil = mysqli_query($koneksi, "SELECT * from tmhs order by id_mhs desc");
+	    		while($data = mysqli_fetch_array($tampil)) :
+
+	    	?>
+	    	<tr>
+	    		<td><?=$no++;?></td>
+	    		<td><?=$data['nim']?></td>
+	    		<td><?=$data['nama']?></td>
+	    		<td><?=$data['alamat']?></td>
+	    		<td><?=$data['prodi']?></td>
+	    		<td>
+	    			<a href="index.php?hal=edit&id=<?=$data['id_mhs']?>" class="btn btn-warning"> Edit </a>
+	    			<a href="index.php?hal=hapus&id=<?=$data['id_mhs']?>" 
+	    			   onclick="return confirm('Apakah yakin ingin menghapus data ini?')" class="btn btn-danger"> Hapus </a>
+	    		</td>
+	    	</tr>
+	    <?php endwhile; ?>
+	    </table>
+
+	  </div>
+	</div>
+
+</div>
+
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
